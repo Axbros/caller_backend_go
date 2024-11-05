001edcb11d6e740c23650ea4550529e2b08cf91b
Axbro <axbros@sina.com>
2024-11-05 10:56:57 +0800

    add sms into user table

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	ggorm.Model `gorm:"embedded"` // embed id and time
-
-	MachineCode string `gorm:"column:machine_code;type:varchar(32)" json:"machineCode"`
+	MachineCode string            `gorm:"column:machine_code;type:varchar(32)" json:"machineCode"`
+	Sms         string            `gorm:"column:sms;type:tinyint(1)" json:"sms"`
 }
 
 // TableName table name
